1d0cc0e5aa58d420e803289e55cb7570e6297a5e
sidereal <holasoyserg@gmail.com>
2022-04-24 15:39:10 -0500

    ignore

--- Covid-Vaccine-Tracker/UI/RecoveryForm.cs
+++ Covid-Vaccine-Tracker/UI/RecoveryForm.cs
@@ -14,10 +14,12 @@ namespace Covid_Vaccine_Tracker.UI
 {
     public partial class RecoveryForm : Form
     {
-        bool usernameRecovery = false, passwordRecovery = false;
+        private bool usernameRecovery = false, passwordRecovery = false;
         (bool, string) IsValid;
         string appTitle = "Covid Vaccine Tracker";
+        private bool errorOccurred = default;
         List<User_Type> accountTypes = new List<User_Type>();
+        User user = new User();
         public RecoveryForm()
         {
             InitializeComponent();
@@ -71,7 +73,6 @@ namespace Covid_Vaccine_Tracker.UI
                     ClearBtn.Top -= 75;
                     SubmitBtn.Top -= 75;
                     this.Height -= 75;
-                    AnwserTxt.Enabled = false;
                 }
 
                 // if username recovery set InputLbl and InputLbl2 text with the InputLbl2.Text = "Firt Name" ..etc
@@ -80,6 +81,28 @@ namespace Covid_Vaccine_Tracker.UI
                 throw ex;
             }
 
+        }
+        public (bool, string) CheckForm(ref int tbx)
+        {
+            // add the validation for your story here see provider form UpdateAccountForm and login form for example of checkform method
+            // note that i started implementing the new error messages in the provider and started on the login form
+            bool valid = true;
+            string errMsg = string.Empty;
+
+            if (string.IsNullOrEmpty(InputTxt1.Text))
+            {
+                valid = false;
+                errMsg = Errors.GetInputErrorMsg(0, "Username");
+                tbx = 0;
+            }
+            else if (AccountCbx.SelectedIndex <= -1)
+            {
+                valid = false;
+                errMsg = Errors.GetInputErrorMsg(11, "Account type");
+                tbx = 1;
+            }
+
+            return (valid, errMsg);
         }
         private void ClearBtn_Click(object sender, EventArgs e)
         {
@@ -91,6 +114,7 @@ namespace Covid_Vaccine_Tracker.UI
             InputTxt2.Text = string.Empty;
             AccountCbx.SelectedIndex = -1;
             AnwserTxt.Text = string.Empty;
+            AnwserLbl.Text = "Your answer";  
             // give input1Txt focus
             InputTxt1.Focus();
         }
@@ -105,10 +129,27 @@ namespace Covid_Vaccine_Tracker.UI
                 IsValid = CheckForm(ref Tbx);
                 if (IsValid.Item1)
                 {
-                    AnwserTxt.Enabled = true;
+                    
+                    try
+                    {
+                        if (accountTypes[AccountCbx.SelectedIndex].UserType == "CDC User")
+                            CreateUser("CDC");
+                        else if (accountTypes[AccountCbx.SelectedIndex].UserType == "Healthcare Provider")
+                            CreateUser("Provider");
+                        if(!errorOccurred)
+                        {
+                            if (VerifyUsername())
+                            {
+                                //
+                            }
+                            else DisplayError("That username doesn't exist.", appTitle);
+
+                        }
+                    } catch(Exception ex) { DisplayError(ex.Message, appTitle); }
+
                 }
-                else
-                    SetErrorPv(Tbx, IsValid.Item2);
+                else SetErrorPv(Tbx, IsValid.Item2);
+
                 // use UserDB to check that the username is in the database and get the user id associated with username
                 // then use the id to get the question from the database using one of the SecurityQuestionDB class methods 
 
@@ -138,6 +179,38 @@ namespace Covid_Vaccine_Tracker.UI
                 // Errors.GetGeneralErrorMsg(33, "Patient", "Patient Id") returns a string with Patient and Patient Id inserted into string
             }
         }
+        private void CreateUser(string type)
+        {
+            try
+            {
+                user.username = InputTxt1.Text.Trim();
+                switch (type)
+                {
+                    case "CDC":
+                        user.user_type = "CDC";
+                        break;
+                    case "Provider":
+                        user.user_type = "Provider";
+                        break;
+                }
+            } catch(Exception ex)
+            {
+                errorOccurred = true;
+                throw ex;
+            }
+
+        }
+        private bool VerifyUsername()
+        {
+            //check if username exists.
+            bool usernameFound = default;
+            string username = user.username;
+            try
+            {
+                usernameFound = UserDB.VerifyUsername(username);
+            } catch(Exception ex) { DisplayError(ex.Message, appTitle); }
+            return usernameFound;
+        }
         private void SetErrorPv(int tbx, string emsg)
         {
             switch(tbx)
@@ -166,6 +239,41 @@ namespace Covid_Vaccine_Tracker.UI
             MessageBox.Show(msg, title, MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
+        // When user clicks the security question box, it will validate the data entries and retrieve the pertaining questions, otherwise it will set errors. 
+        private void AnwserTxt_Enter(object sender, EventArgs e)
+        {
+            //ResetErrorPv();
+            //// Sergio's section
+            //// when the user submits check the recovery type
+            //if (passwordRecovery)
+            //{
+            //    int Tbx = -1;
+            //    IsValid = CheckForm(ref Tbx);
+            //    if (IsValid.Item1)
+            //    {
+            //        if (InputTxt1.Text == "sergio") AnwserLbl.Text = "What year was your grandfather born?";
+            //        else if (InputTxt1.Text == "ochoa") AnwserLbl.Text = "What was your favorite teacher's name?";
+            //        else AnwserLbl.Text = "Your answer";
+            //    }
+            //    else
+            //    {
+            //        SetErrorPv(Tbx, IsValid.Item2);
+            //    }
+            //    // use UserDB to check that the username is in the database and get the user id associated with username
+            //    // then use the id to get the question from the database using one of the SecurityQuestionDB class methods 
+
+            //    // if the anwser matches then call the UpdateAccountForm and pass in the username
+
+            //    // if anwser dont match display error message with DisplayError and use the new Errors class
+            //    // you might have to look up the specific message code but a example of the new way to get error messages and
+            //    // display through a DisplayErorrMethod would be :
+            //    // DisplayError(Errors.GetGeneralErrorMsg(33, "Patient", "Patient Id"), AppTitle);
+            //    // Errors.GetGeneralErrorMsg(33, "Patient", "Patient Id") returns a string with Patient and Patient Id inserted into string
+
+
+            //}
+        }
+
         //private void InputTxt1_Leave(object sender, EventArgs e)
         //{
         //    //check if username exists. If yes, gets the security question, if not, displays an error. 
@@ -177,28 +285,5 @@ namespace Covid_Vaccine_Tracker.UI
 
         //    }
         //}
-
-        public (bool,string) CheckForm(ref int tbx)
-        {
-            // add the validation for your story here see provider form UpdateAccountForm and login form for example of checkform method
-            // note that i started implementing the new error messages in the provider and started on the login form
-            bool valid = true;
-            string errMsg = string.Empty;
-
-            if (string.IsNullOrEmpty(InputTxt1.Text))
-            {
-                valid = false;
-                errMsg = Errors.GetInputErrorMsg(0, "Username");
-                tbx = 0;
-            }
-            else if (AccountCbx.SelectedIndex <=-1)
-            {
-                valid = false;
-                errMsg = Errors.GetInputErrorMsg(11, "Account type");
-                tbx = 1;
-            }
-
-            return (valid, errMsg);
-        }
     }
 }
