f9b4cbdd6bf0938c34b69bba357d600ca51a29b6
Zach Palmer <apex1421@outlook.com>
2022-04-28 14:04:19 -0500

    dd

--- Covid-Vaccine-Tracker/Program.cs
+++ Covid-Vaccine-Tracker/Program.cs
@@ -18,7 +18,7 @@ namespace Covid_Vaccine_Tracker
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm());
+            Application.Run(new ViewForm());
             //Application.Run(new SignupForm("Provider"));
         }
     }
